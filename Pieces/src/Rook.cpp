9ee977804f273acb4327267496f6a379334c89b0
Jordan <jordanhoang@live.ca>
2020-03-09 02:59:29 -0700

    Add 1 test for rook, fixed main game loop

--- Pieces/src/Rook.cpp
+++ Pieces/src/Rook.cpp
@@ -2,6 +2,7 @@
 // Created by jordan on 2/4/20.
 //
 
+#include <ChessCollision.h>
 #include "Rook.h"
 
 
@@ -9,13 +10,7 @@ Rook::Rook(Color color, char16_t symbol) : Piece{color, symbol} { }
 
 bool Rook::movePiece(ChessCoordinate a, ChessCoordinate b, ChessBoard &chess_board_) {
 
-
-
-
-
-
-
-    return true;
+    return ChessCollision::isPathClear(a,b, chess_board_ );
 }
 void Rook::print() {
     std::cout << "I'm a rook\n";
