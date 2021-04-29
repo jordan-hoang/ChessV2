51676a0cb882abf6bcd4afe4f5d6ca7173f569c7
Jordan Hoang <jordanhoang@live.ca>
2021-04-29 01:35:20 -0700

    Made the chessGame run in a seperate thread

--- ChessPiece/src/NullPiece.cpp
+++ ChessPiece/src/NullPiece.cpp
@@ -13,7 +13,8 @@ NullPiece::NullPiece(Color color, char16_t symbol) : Piece{color, symbol}{
 void NullPiece::print() {
     std::cout << "I'm a nullpiece";
 }
+
 bool NullPiece::movePiece(ChessCoordinate a, ChessCoordinate b, ChessBoard &chess_board_) {
-    std::cout << "MOVING EMPTY SQUARE!\n";
+    std::cout << "MOVING EMPTY SQUARE!, in NULLPIECE.cpp\n";
     return false;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <ChessController.h>
+#include <thread>
 
+static pthread_cond_t SHUTDOWN_CONDL = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t SHUTDOWN_MUTEX = PTHREAD_MUTEX_INITIALIZER; //Lock that determines if the loop keeps going
 
 int main() {
 
+
     ChessController a;
-    a.playGame();
+    a.run();
+
+    // Sample move say is b6,b5 or can be a6,a4.
+
+    //a.playGame();
+    pthread_cond_wait(&SHUTDOWN_CONDL, &SHUTDOWN_MUTEX); // Signals the thread that called this function to 'sleep.' never ends.
+
+
 
     return 0;
 }
