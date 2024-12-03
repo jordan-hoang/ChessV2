0b458bc04611a93b23390c6d9506c9f1db605c66
Jordan <36338184+jordan-hoang@users.noreply.github.com>
2024-12-02 21:25:15 -0800

    messages cleanup

--- ChessPiece/src/NullPiece.cpp
+++ ChessPiece/src/NullPiece.cpp
@@ -2,19 +2,14 @@
 // Created by jordan on 2/4/20.
 //
 
+// A class that represents an empty space on a chessBoard,
 #include "NullPiece.h"
 
-
-NullPiece::NullPiece(Color color, char16_t symbol) : Piece{color, symbol}{
-
-}
-
+NullPiece::NullPiece(Color color, char16_t symbol) : Piece{color, symbol}{}
 
 void NullPiece::print() {
-    std::cout << "I'm a nullpiece";
 }
 
 bool NullPiece::movePiece(ChessCoordinate a, ChessCoordinate b, ChessBoard &chess_board_) {
-    std::cout << "MOVING EMPTY SQUARE!, in NULLPIECE.cpp\n";
     return false;
 }
